ea2c66929181d63df924bb3dd326af335386af51
Bernardo <berrfarias@gmail.com>
2023-07-18 18:41:32 +1000

    astro project

---
+++ src/helpers/index.js
@@ -0,0 +1,8 @@
+export const formatDate = fecha => {
+    const opciones = {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit'
+    }
+    return new Date(fecha).toLocaleDateString('es-ES', opciones)
+}
